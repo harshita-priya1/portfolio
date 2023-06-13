f1b865457a268a11d8d8c886a1204303a70e721c
HARSHITA PRIYA <harshitapriyaph5@gmail.com>
2023-06-14 01:14:29 +0530

    Projects page added

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
       <Home/>
       <About/>
       <Techs/>
-      {/* <Projects/> */}
+      <Projects/>
       <Contacts/>
     </div>
   );
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,7 +4,10 @@ import "./Projects.css"
 const Projects = () => {
     return(
         <div className="project">
-
+            <div className="project-left">
+            </div>
+            <div className="project-right">
+            </div>
         </div>
     )
 }
