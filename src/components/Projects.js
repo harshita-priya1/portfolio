2feee57aa74092219eb8e9eba9dd811156d7e214
HARSHITA PRIYA <harshitapriyaph5@gmail.com>
2023-06-14 02:34:11 +0530

    Projects page improved

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,6 +5,17 @@ const Projects = () => {
     return(
         <div className="project">
             <div className="project-left">
+                <div className="project-desc">
+                    <div className="project-title">
+                        Project Title
+                    </div>
+                    <p className="project-text">A short description of the project</p>
+                    <button className="project-btn">View Project →</button>
+                    <div className="arrow-button">
+                        <button className="arrow-left">⇦</button>
+                        <button className="arrow-right">⇨</button>
+                    </div>
+                </div>
             </div>
             <div className="project-right">
             </div>
