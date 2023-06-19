075e75ee116115751394b9ed6cb1de9b34a0a987
HARSHITA PRIYA <harshitapriyaph5@gmail.com>
2023-06-19 16:04:09 +0530

    About Text changed

--- src/components/About.js
+++ src/components/About.js
@@ -9,24 +9,19 @@ const About = () => {
                 <div className="a1">Get to Know Me!</div>
                 <div className="a2">Hi,</div>
                 <div className="a3">
-                    <p>I'm <b>Harshita Priya</b> , passionate about creating engaging and responsive websites that deliver an exceptional user experience. 
-                    I specialize in designing and developing custom websites for small businesses, startups, and individuals who are looking to establish a strong online presence. 
+                    <p>I'm <b>Harshita Priya</b> , passionate about creating engaging websites and apps that deliver exceptional visuals and functionality. 
                     </p>
 
-                    <p>Whether you need a website for your business, a personal blog, or an e-commerce platform, I am here to help. 
-                        My approach to web design and development is collaborative and tailored to your specific needs. 
+                    <p>Whether you need an app or a website for your business, a personal blog, or an e-commerce platform, I am here to help. 
+                        My approach to design and development is collaborative and tailored to your specific needs. 
                         I take the time to understand your business objectives, target audience, 
-                        and branding guidelines to deliver a website that reflects your unique style and personality.
+                        and branding guidelines to deliver a product that reflects your unique style and personality.
                     </p>
 
-                    <p>In addition to web development and design, I also provide ongoing maintenance and support services to
-                        ensure that your website stays up to date and runs smoothly. I am committed to providing exceptional
-                        customer service and ensuring that my clients are satisfied with the end result.
+                    <p>In addition to web development and design, I also do have some experience with machine learning and AI, which can be integrated with the product.
                     </p>
 
-                    <p>If you're looking for a freelance web developer and designer who can bring your vision to life, 
-                        look no further. Let's work together to create a website that exceeds your expectations.
-                    </p>
+                    <p>Scroll Down to know more about me!</p>
                 </div>
             </div>
         </div>
